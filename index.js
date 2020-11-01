164fe77c72841366a1bc879d2cdd86807e8145cb
fish-tian <tshl359250296@qq.com>
2020-11-01 21:29:32 +0800

    服务端可以广播消息到所有用户，聊天1.0版本完成

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected');
     
+    // socket 接收到 chat message
+    socket.on('chat message', (message) => {
+        console.log("message: " + message);
+        io.emit('chat message broadcast', message);
+    });
+
+    // socket 接收到断开连接
     socket.on('disconnect', () => {
         console.log('a user disconnected');
     });
-})
+});
 
 http.listen(3000, () => {
   console.log('listening on *:3000');
